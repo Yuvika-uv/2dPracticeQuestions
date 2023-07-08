22cfdb3ba31522643ffe14d69ca177385bd1bf24
Yuvika-uv <126947393+Yuvika-uv@users.noreply.github.com>
2023-07-08 20:20:35 +0530

    Create MatrixDiagonalSum.java

---
+++ MatrixDiagonalSum.java
@@ -0,0 +1,18 @@
+public class MatrixDiagonalSum {
+    public static void main(String[] args) {
+
+    }
+    public int diagonalSum(int[][] mat) {
+        int sum=0;
+        int k=mat.length;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat.length;j++){
+                if(i==j || i+j==mat.length-1){
+                    sum+=(mat[i][j]);
+                }
+
+            }
+        }
+        return sum;//return sum
+    }
+}
